2b0bcd698f20dd23435e497bb99d4db7686b2f5f
Sebastián Giraldo <sebastian.giraldo@talosdigital.com>
2020-10-19 09:41:36 -0500

    Project setup

---
+++ TalosiOSApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TalosiOSApp
+//
+//  Created by Sebastian Giraldo  on 10/19/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
